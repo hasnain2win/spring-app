abb3fc42c51a858c9edf0b0e39c7607d1f1a9262
020164@sundaramfiance.in <020164@sundaramfinance.in>
2023-05-23 12:42:11 +0530

    placed appConfig in config package

--- src/main/java/com/vam/hassan/config/AppConfig.java
+++ src/main/java/com/vam/hassan/config/AppConfig.java
@@ -1,4 +1,4 @@
-package com.vam.hassan;
+package com.vam.hassan.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
